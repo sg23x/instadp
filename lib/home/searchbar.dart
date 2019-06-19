636c7e80fc7b06215956180b8bee122f833421b4
sg23x <soumyagupta2307@gmail.com>
2019-06-19 19:05:39 +0530

    fixed location icon

--- lib/home/searchbar.dart
+++ lib/home/searchbar.dart
@@ -12,22 +12,48 @@ class _SeachBarState extends State<SeachBar> {
       color: Color(0xff0875b5),
       height: 70,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          SizedBox(
-            width: 10,
-          ),
-          Flexible(
-            child: TextField(
-              decoration: InputDecoration(
-                alignLabelWithHint: false,
-                prefixIcon: Image.asset(
-                  "assets/magnifying_glass.png",
-                  scale: 1.5,
+          Container(
+            width: MediaQuery.of(context).size.width * 0.53,
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      alignLabelWithHint: false,
+                      prefixIcon: Image.asset(
+                        "assets/magnifying_glass.png",
+                        scale: 1.5,
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: "Search Services",
+                    ),
+                  ),
                 ),
-                fillColor: Colors.white,
-                filled: true,
-                hintText: "Search Services",
-              ),
+              ],
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.39,
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      alignLabelWithHint: false,
+                      suffixIcon: Image.asset(
+                        "assets/location.jpg",
+                        scale: 40,
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: "Location?",
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
